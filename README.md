e1ee7df3015d8dadd4d7d05fa5b4faef72f34474
aditya1746 <adityarajsinghgour17@gmail.com>
2021-05-28 13:30:45 +0530

    completed!!!!!!

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # tic_tac_toe_openCV
+
+So, in order to play this game on your system you must have python, and two of it's libraries numpy and openCV
+
+As soon as you will run main.py file a window will pop in which the player no. 1 has to select his/her symbol
+and based on that the second player will be given the other symbol
+
+To select something you must double click on that
+
+After the player 1 selcts his/her symbol the game window will appear, at the bottom of that window a text is written to convey to the player's that whose turn it is.
+
+Then players can start filling the boxes by double clickingg in that box.
+
+Once the game is over a text will pop up at the top with the message "Game Over !!" , and the result will be displayed at the bottom. Then to close press any key
+
+If after selecting the symbol, at any point of time you want to close the game press esccape key and then press any key
--- main.py
+++ main.py
@@ -10,13 +10,15 @@ winner = 0
 curr_player = 0
 cnt = 0
 
-rows, cols = (5, 5)
-arr = [[0]*cols]*rows
+arr = [[0,0,0],[0,0,0],[0,0,0]]
 
 symbol_x = cv2.imread('symbol_x.jpg')
 symbol_o = cv2.imread('symbol_o.jpg')
 p1_box = cv2.imread('p1_box.jpg')
 p2_box = cv2.imread('p2_box.jpg')
+p1_win = cv2.imread('p1_win.jpg')
+p2_win = cv2.imread('p2_win.jpg')
+draw = cv2.imread('draw.jpg')
 
 def whichArea(x,y):
 
@@ -112,6 +114,43 @@ def whichArea(x,y):
         cnt = cnt+1
         arr[2][2] = curr_player
 
+def checkWinner():
+
+    global winner
+
+    for row in arr:
+
+        if(row[0]!=0 and row.count(row[0])==3):
+            winner = row[0]
+            
+            if(row[0]==arr[0][0]):
+                cv2.line(img,(111,151),(401,151),(255,0,0),3)
+            elif(row[0]==arr[1][0]):
+                cv2.line(img,(111,256),(401,256),(255,0,0),3)
+            else:
+                cv2.line(img,(111,361),(401,361),(255,0,0),3)
+            break
+    
+    if(arr[0][0]!=0 and arr[0][0]==arr[1][0] and arr[0][0]==arr[2][0]):
+        winner = arr[0][0]
+        cv2.line(img,(151,111),(151,401),(255,0,0),3)
+    
+    if(arr[0][1]!=0 and arr[0][1]==arr[1][1] and arr[0][1]==arr[2][1]):
+        winner = arr[0][1]
+        cv2.line(img,(256,111),(256,401),(255,0,0),3)
+
+    if(arr[0][2]!=0 and arr[0][2]==arr[1][2] and arr[0][2]==arr[2][2]):
+        winner = arr[0][2]
+        cv2.line(img,(361,111),(361,401),(255,0,0),3)
+
+    if(arr[0][0]!=0 and arr[0][0]==arr[1][1] and arr[0][0]==arr[2][2]):
+        winner = arr[0][0]
+        cv2.line(img,(111,111),(401,401),(255,0,0),3)
+
+    if(arr[2][0]!=0 and arr[2][0]==arr[1][1] and arr[2][0]==arr[0][2]):
+        winner = arr[1][1]
+        cv2.line(img,(111,401),(401,111),(255,0,0),3)
+
 def startCallback(event,x,y,flags,param):
 
     global player1,player2,symbolSelected
@@ -138,10 +177,10 @@ def playgroundCallback(event,x,y,flags,param):
     if(event==cv2.EVENT_LBUTTONDBLCLK):
 
         whichArea(x,y)
+
+        checkWinner()
     
     
-
-
 startPage = np.zeros((512,512,3),np.uint8)
 cv2.namedWindow('start_page')
 cv2.setMouseCallback('start_page',startCallback)
@@ -157,6 +196,8 @@ while(symbolSelected==0):
     cv2.imshow('start_page',startPage)
     cv2.waitKey(1)
 
+cv2.destroyWindow('start_page')
+
 img = np.zeros((512,512,3),np.uint8)
 cv2.namedWindow('playground')
 cv2.setMouseCallback('playground',playgroundCallback)
@@ -165,7 +206,7 @@ cv2.rectangle(img,(96,96),(416,416),(255,255,255),5)
 cv2.rectangle(img,(201,96),(311,416),(255,255,255),5)
 cv2.rectangle(img,(96,201),(416,311),(255,255,255),5)
 
-while(winner==0):
+while(winner==0 and cnt<9):
 
     if(cnt%2==0):
         curr_player = player1
@@ -178,6 +219,17 @@ while(winner==0):
 
     if(cv2.waitKey(10) & 0xFF == 27):
         break
-    
-#cv2.waitKey(0)
+
+cv2.putText(img,'Game Over !!',(136,60),font,1,(255,0,0),1,cv2.LINE_AA)
+
+if(cnt==9):
+    img[455:511,0:280] = draw
+elif(curr_player==player1):
+    img[455:511,0:280] = p1_win
+else:
+    img[455:511,0:280] = p2_win
+
+cv2.imshow('playground',img)
+
+cv2.waitKey(0)
 cv2.destroyAllWindows()
